9886f127fc760e610aeeda55de75ef2ee18f8755
Vlad_Oboturov <vlad100493@gmail.com>
2024-02-19 21:51:03 +0300

    start project

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>Header</header>
+    <nav v-if="this.$route.path === '/'">
+      <br />
+      <router-link to="/main">Main</router-link>
+      <br />
+      <br />
+      <router-link to="/about">About</router-link>
+      <br />
+      <br />
+      <router-link to="/ui-kit">UI Kit</router-link>
+      <br />
+      <br />
+    </nav>
+    <router-view />
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
---
+++ src/pages/main/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <main class="main">
+        Main
+    </main>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss">
+.main {
+    width: 100vw;
+    height: 100vh;
+    background-color: aquamarine;
+}
+</style>
