2441fd6a24a694c3bff40cb47c3b557d8cbf4052
Vlad_Oboturov <vlad100493@gmail.com>
2024-02-21 09:03:28 +0300

    doing hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="hero">
     <img class="hero__img" src="../assets/images/img.jpg" alt="" />
-    <div class="next-block__big-text">
-      <span class="next-block__letter">J</span>
-      <span class="next-block__letter">E</span>
-      <span class="next-block__letter">F</span>
-      <span class="next-block__letter">F</span>
-      <span class="next-block__letter">R</span>
-      <span class="next-block__letter">E</span>
-      <span class="next-block__letter next-block__letter--space">Y </span>
-      <span class="next-block__letter">M</span>
-      <span class="next-block__letter">A</span>
-      <span class="next-block__letter">R</span>
-      <span class="next-block__letter">T</span>
-      <span class="next-block__letter">I</span>
-      <span class="next-block__letter">N</span>
+    <div class="hero__name">
+      <div class="hero__word">
+        <span v-for="letter in name[0]">{{ letter }}</span>
+      </div>
+      <div class="hero__word">
+        <span v-for="letter in name[1]">{{ letter }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: ["JEFFREY", "MARTIN"],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
