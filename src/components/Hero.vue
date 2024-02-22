395643d2e7887d0de2e2502c89a651dc705bd3be
Vlad_Oboturov <vlad100493@gmail.com>
2024-02-22 15:18:38 +0300

    try to fix fonts problem with team lead

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,10 +3,10 @@
     <img class="hero__img" src="../assets/images/img.jpg" alt="" />
     <div class="hero__name">
       <div class="hero__word">
-        <span v-for="letter in name[0]">{{ letter }}</span>
+        <span v-for="letter, i in name[0]" :key="i">{{ letter }}</span>
       </div>
       <div class="hero__word">
-        <span class="hero__letter" v-for="letter in name[1]">{{ letter }}</span>
+        <span class="hero__letter" v-for="letter, i in name[1]" :key="i">{{ letter }}</span>
       </div>
     </div>
   </div>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,4 +17,5 @@ export default {};
 </script>
 
 <style lang="scss">
+@import "~/src/assets/styles/base/base.scss";
 </style>
