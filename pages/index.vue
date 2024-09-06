a9b8a7565b9b29543ba20dcd8c8c9d22e7fcdc7f
vladislav oboturov <vlad100493@gmail.com>
2024-09-06 17:01:39 +0300

    fix warnings, did main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>
-    <nuxt-link to="/main">Главная страница </nuxt-link>
-    <br />
-    <br />
-  </div>
+  <main class="main">
+    <Hero />
+    <Quote />
+    <Show />
+    <Upcoming />
+    <Album />
+    <Quotes />
+    <Videos />
+    <Story />
+  </main>
 </template>
+
+<script>
+import Hero from "@/components/Hero";
+import Quote from "@/components/Quote";
+import Show from "@/components/Show";
+import Upcoming from "@/components/Upcoming";
+import Album from "@/components/Album";
+import Quotes from "@/components/Quotes";
+import Videos from "@/components/Videos";
+import Story from "@/components/Story";
+
+export default {
+  components: {
+    Hero,
+    Quote,
+    Show,
+    Upcoming,
+    Album,
+    Quotes,
+    Videos,
+    Story,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~/assets/styles/pages/main";
+</style>
